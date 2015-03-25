9e8f49c9a13a4956bad237d8f476cf6445ffbb54
aacase <aacase@gmail.com>
2015-03-25 16:42:44 -0500

    Page styling for launch page.

--- client/views/launch/launch.js
+++ client/views/launch/launch.js
@@ -3,10 +3,10 @@ Template.launch.rendered=function(){
 }
 
 Template.launch.helpers({
-	pacificTime:function(){return moment().tz('America/Los_Angeles').format("h:mm A");},
-	mountainTime:function(){return moment().tz('America/Denver').format("h:mm A");},
-	centralTime:function(){return moment().tz('America/Chicago').format("h:mm A");},
-	easternTime:function(){return moment().tz('America/New_York').format("h:mm A");},
+	pacificTime:function(){return moment().tz('America/Los_Angeles').format("h:mm a");},
+	mountainTime:function(){return moment().tz('America/Denver').format("h:mm a");},
+	centralTime:function(){return moment().tz('America/Chicago').format("h:mm a");},
+	easternTime:function(){return moment().tz('America/New_York').format("h:mm a");},
 	localTime:function(){return moment().format("h:mm")},
 	date:function(){return moment().format('dddd, MMMM D')}
 });
--- server/config/security.js
+++ server/config/security.js
@@ -3,3 +3,5 @@ BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
 
 BrowserPolicy.content.allowFontDataUrl();
+
+BrowserPolicy.content.allowOriginForAll('http://i.imgur.com/QXcy934.jpg');
