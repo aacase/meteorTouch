f66ad41542de207bafdbf5237738229921f270be
aacase <aacase@gmail.com>
2015-03-30 14:01:23 -0500

    inCall styling continued
    x button goes to home screen.

--- client/views/inCall/inCall.js
+++ client/views/inCall/inCall.js
@@ -2,4 +2,9 @@ Template['inCall'].helpers({
 });
 
 Template['inCall'].events({
+
+	//you're going to need to use if statments here to determine where the x button routes. 
+	'click .exitCall' : function () {
+	  Router.go('/homescreen');
+	},
 });
